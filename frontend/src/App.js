b57c6210d65ae5b032cc7c9140584d0b29c5b0da
goutambarve <barvegoutam16@gmail.com>
2026-04-25 02:37:47 +0530

    Connected frontend to Render backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -74,7 +74,7 @@ function App() {
     setLoading(true);
     
   try {
-    const response = await fetch("https://ai-code-reviewer-1-5oek.onrender.com/", {
+    const response = await fetch("https://ai-code-reviewer-1-5oek.onrender.com/analyze", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -110,7 +110,7 @@ localStorage.setItem("history", JSON.stringify(updatedHistory));
   // FIX CODE
   const fixCode = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:5001/fix", {
+      const response = await fetch("https://ai-code-reviewer-1-5oek.onrender.com/fix", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
