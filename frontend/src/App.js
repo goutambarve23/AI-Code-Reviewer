c787bfc91d2cc42f9c1f0421dcbef3710aac65a1
goutambarve <barvegoutam16@gmail.com>
2026-04-25 02:33:48 +0530

    Connected frontend to Render backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -74,7 +74,7 @@ function App() {
     setLoading(true);
     
   try {
-    const response = await fetch("http://127.0.0.1:5001/analyze", {
+    const response = await fetch("https://ai-code-reviewer-1-5oek.onrender.com/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
